7613e52d35d333bf624e55afa20d06bff65621fe
Dmitriy <fstpain@gmail.com>
2024-07-22 23:24:50 +0300

    t1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {Test1Component} from "./test1/test1.component";
 
 @Component({
   selector: 'app-root',
---
+++ src/app/test1/test1.component.ts
@@ -0,0 +1,13 @@
+import {Component} from "@angular/core";
+
+@Component({
+  selector: 'app-test1',
+  standalone: true,
+  templateUrl: 'test1.component.html',
+  styleUrls: ['test1.component.css']
+})
+
+export class Test1Component {
+
+
+}
