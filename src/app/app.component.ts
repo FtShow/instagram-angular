40ae6668cf5a9cfc65f543df1a2d6f469be90bb0
Dmitriy <fstpain@gmail.com>
2024-07-24 20:26:40 +0300

    binding test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,25 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import {Test1Component} from "./test1/test1.component";
-import {Test2Component} from "./test2/test2.component";
+import { Component } from '@angular/core'
+import { RouterOutlet } from '@angular/router'
+import { Test1Component } from './test1/test1.component'
+import { Test2Component } from './test2/test2.component'
+
+type iUser = {
+  age: number
+  name: string
+}
 
 @Component({
-  selector: 'app-root',
+  selector: 'inst-root',
   standalone: true,
   imports: [RouterOutlet, Test1Component, Test2Component],
   templateUrl: './app.component.html',
-  styleUrl: 'app.component.scss'
+  styleUrl: 'app.component.scss',
 })
 export class AppComponent {
-  title = 'instagram-angular';
+  appTitle: string = 'instagram-angular'
+  user: iUser = {
+    age: 32,
+    name: 'Ivan',
+  }
+  isAppLoading = true
 }
