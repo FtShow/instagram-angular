2c86b6c87ad5547d2bee3db9d6a7d37aa1a236fc
Dmitriy <fstpain@gmail.com>
2024-08-13 23:40:41 +0300

    ngSwitch

--- src/app/parent/parent.component.ts
+++ src/app/parent/parent.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core'
 import { ChildComponent, Grade } from './child/child.component'
-import { JsonPipe, NgForOf, NgIf } from '@angular/common'
+import { JsonPipe, NgForOf, NgIf, NgSwitch, NgSwitchCase, NgSwitchDefault } from '@angular/common'
+import { FormsModule } from '@angular/forms'
 
 export interface Address {
   city: string
@@ -22,58 +23,10 @@ export interface Lessons {
 @Component({
   selector: 'inst-parent',
   standalone: true,
-  imports: [ChildComponent, JsonPipe, NgForOf, NgIf],
+  imports: [ChildComponent, JsonPipe, NgForOf, NgIf, FormsModule, NgSwitch, NgSwitchCase, NgSwitchDefault],
   templateUrl: './parent.component.html',
   styleUrl: './parent.component.scss',
 })
 export class ParentComponent {
-  isLoading = true
-  lessons: Lessons[] = [
-    {
-      id: 0,
-      title: 'Math',
-      weekGrades: [
-        {
-          id: 0,
-          gradeItem: 5,
-        },
-        {
-          id: 1,
-          gradeItem: 3,
-        },
-        {
-          id: 2,
-          gradeItem: 4,
-        },
-      ],
-    },
-    {
-      id: 1,
-      title: 'Physic',
-      weekGrades: [
-        {
-          id: 0,
-          gradeItem: 1,
-        },
-        {
-          id: 1,
-          gradeItem: 67,
-        },
-        {
-          id: 2,
-          gradeItem: 2,
-        },
-      ],
-    },
-  ]
-
-  constructor() {
-    setTimeout(() => {
-      this.isLoading = false
-    }, 3000)
-  }
-
-  getGrade(grade: string) {
-    console.log(grade)
-  }
+  value = ''
 }
